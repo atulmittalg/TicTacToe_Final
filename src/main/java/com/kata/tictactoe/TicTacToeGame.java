af2bccc5bef8cecf1ef4a49007e368c74358d6c0
2020-DEV-061 <55511902+atulmittalg@users.noreply.github.com>
2020-03-23 22:57:45 +0100

    Player X as winner when first row fully occupied by X.

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -4,10 +4,16 @@ public class TicTacToeGame {
     private char[][] gameBoardLayout = new char[3][3];
     private char currentPlayer;
 
-    public void playTurnAt(final int positionX, final int positionY) throws PositionAlreadyInUseException {
+    public String playTurnAt(final int positionX, final int positionY) throws PositionAlreadyInUseException {
         checkIsPositionAlreadyFilled(positionX, positionY);
         currentPlayer = getCurrentPlayerToBePlayed();
         gameBoardLayout[positionX][positionY] = currentPlayer;
+        if ((getPlayerAt(0,0) == 'X' ) &&
+                (getPlayerAt(0,0) == getPlayerAt(0,1)) &&
+                (getPlayerAt(0,0) == (getPlayerAt(0,2)))) {
+            return "Player X is the Winner";
+        }
+        return null;
     }
 
     private void checkIsPositionAlreadyFilled(final int positionX, final int positionY) throws PositionAlreadyInUseException {
