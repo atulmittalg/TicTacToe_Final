76e97e66d6e0438fef2d648c735651aa87cccc29
2020-DEV-061 <55511902+atulmittalg@users.noreply.github.com>
2020-03-23 20:16:01 +0100

    Player O is set when second turn is played.

--- src/main/java/com/kata/tictactoe/TicTacToeGame.java
+++ src/main/java/com/kata/tictactoe/TicTacToeGame.java
@@ -2,9 +2,11 @@ package com.kata.tictactoe;
 
 public class TicTacToeGame {
     private char[][] gameBoardLayout = new char[3][3];
+    private char currentPlayer;
 
     public void playTurnAt(final int positionX, final int positionY) {
-        gameBoardLayout[positionX][positionY] = 'X';
+        currentPlayer = currentPlayer == 'X' ? 'O' : 'X';
+        gameBoardLayout[positionX][positionY] = currentPlayer;
     }
 
     public char getPlayerAt(final int positionX, final int positionY) {
